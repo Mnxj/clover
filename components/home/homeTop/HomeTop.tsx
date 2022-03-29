f1532dbcefbab9e7feca3ff56f18952b90e4c3af
tianhang <tianhang.shi@thoughtworks.com>
2022-03-29 23:33:02 +0800

    [clover] ST-3204 fix: add rel="noreferrer".

--- components/home/homeTop/HomeTop.tsx
+++ components/home/homeTop/HomeTop.tsx
@@ -45,19 +45,19 @@ export const HomeTop = () => {
                                 <img className="flipx" src="/images/next-b.svg" alt="flipx"/>
                             </li>
                             <li>
-                                <a href="https://github.com/Mnxj" target="_blank"><img src="/images/github.png" alt="github"/></a>
+                                <a href="https://github.com/Mnxj" rel="noreferrer"  target="_blank"><img src="/images/github.png" alt="github"/></a>
                             </li>
                             <li>
-                                <a href="https://space.bilibili.com/57933284" target="_blank" title="bilibili"><img src="/images/bilibili.png" alt="bilibili"/></a>
+                                <a href="https://space.bilibili.com/57933284" rel="noreferrer"  target="_blank" title="bilibili"><img src="/images/bilibili.png" alt="bilibili"/></a>
                             </li>
                             <li>
-                                <a href="https://music.163.com/#/user/home?id=394247655" target="_blank" title="CloudMusic"><img src="/images/wangyiyun.png" alt="wangyiyun"/></a>
+                                <a href="https://music.163.com/#/user/home?id=394247655" rel="noreferrer"  target="_blank" title="CloudMusic"><img src="/images/wangyiyun.png" alt="wangyiyun"/></a>
                             </li>
                             <li>
-                                <a href="https://www.zhihu.com/people/sao-di-seng-78-11" target="_blank" title="知乎"><img src="/images/zhihu.png" alt="zhihu"/></a>
+                                <a href="https://www.zhihu.com/people/sao-di-seng-78-11" rel="noreferrer"  target="_blank" title="知乎"><img src="/images/zhihu.png" alt="zhihu"/></a>
                             </li>
                             <li>
-                                <a href="https://blog.csdn.net/Adim12?spm=1000.2115.3001.5343" target="_blank" title="CSDN"><img src="/images/csdn.png" alt="csdn"/></a>
+                                <a href="https://blog.csdn.net/Adim12?spm=1000.2115.3001.5343" rel="noreferrer"  target="_blank" title="CSDN"><img src="/images/csdn.png" alt="csdn"/></a>
                             </li>
                             <li id="bg-next">
                                 <img src="/images/next-b.svg" alt="next-b"/>
